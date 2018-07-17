d59e78fcb797688d8aacdb2378790c489160af48
Vavan <verinvld@yandex.ru>
2018-07-17 23:10:31 +0300

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+alert ("privet");
+alert ("привет");
+
+function f1(){
+     alert('ты нажал кнопку?)))')
+}
+function f2(){
+     alert('а тут ты жмешь на параграф)))))')
+}
+function f3(){
+     alert('это твоя тачка)))')}
